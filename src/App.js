5dbd2d475dca07734387547ee488779652a596be
Suzukisakae <lethanhvinhspk@gmail.com>
2024-02-19 16:25:40 +0700

    Refactor App component and add new components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import "./style.css";
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>Today I Learned</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        <li className="category">
+          <button className="btn btn-category">Technology</button>
+        </li>
+        <li className="category">
+          <button className="btn btn-category">Science</button>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+function FactList() {
+  return <section className="fact-list">Facts List</section>;
 }
 
 export default App;
